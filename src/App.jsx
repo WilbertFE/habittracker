87665eb3835ddaa66508817bf7a3ab7f9ac3dfdd
WilbertFE <wilbertbernadi699@gmail.com>
2024-01-26 19:15:50 +0700

    Menambahkan isi app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,204 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const defaultHabits = [
+  {
+    id: 1,
+    name: 'Olahraga',
+    checked: true
+  },
+  {
+    id: 2,
+    name: 'Belajar',
+    checked: false
+  },
+  {
+    id: 3,
+    name: 'Membaca',
+    checked: false
+  },
+  {
+    id: 4,
+    name: 'Ngoding',
+    checked: true
+  },
+];
+
+export default function App() {
+
+  const [habits, setHabits] = useState(defaultHabits);
+
+  function handleToggle(id){
+    const newHabits = habits.map((habit) => habit.id === id ? {...habit, checked: !habit.checked} : habit);
+    setHabits(newHabits);
+  }
+
+  function handleDelete(id){
+    const nextHabits = habits.filter((habit) => habit.id !== id);
+    setHabits(nextHabits);
+  }
+
+  function handleClear(){
+    setHabits([]);
+  }
+
+  function handleAdd(value){
+    const newObj = {};
+    newObj.id = habits.length + 1;
+    newObj.name = value;
+    newObj.checked = false;
+
+    const nextHabits = [...habits, newObj];
+    setHabits(nextHabits);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="app" className="font-roboto bg-primary min-h-[1000px]">
+      {/* Awal Header */}
+        <Header habits={habits} />
+      {/* Akhir Header */}
+
+      {/* Awal Lists */}
+        <Main habits={habits} onToggle={handleToggle} onDelete={handleDelete} onClear={handleClear} />
+      {/* Akhir Lists */}
+
+      {/* Awal Footer */}
+      <Footer onAdd={handleAdd} />
+      {/* Akhir Footer */}
+
+    </div>
+  ); 
 }
 
-export default App
+
+// Awal komponen header
+function Header({habits}){
+
+  return (
+    <header className="pt-12">
+         <div className="container">
+          <div className="flex flex-wrap mx-4">
+            <Title />
+            <Tracker habits={habits} />
+          </div>
+         </div>
+    </header>
+  );
+}
+
+function Title(){
+  return (
+    <div className="w-full my-4">
+     <h1 className="pt-4 text-light text-5xl text-center font-bold">Habit Tracker</h1>
+     <p className="text-center text-light text-base text-opacity-90 lowercase">ciptakan hari yang produktif</p>
+    </div>
+  );
+}
+
+function Tracker({habits}){
+  const totalLength = habits.length;
+  const checkedLength = habits.filter((habit) => habit.checked).length;
+  const trackerValue = `${Math.round((checkedLength / totalLength) * 100)}%`;
+
+  return (
+    <div className="w-full py-4">
+      <h1 className="text-center text-light text-xl bg-dark/70 p-2 rounded-lg">Kebiasaan Selesai: <strong>{totalLength > 0 ? trackerValue : 'Kebiasaan kosong'}</strong></h1>
+    </div>
+  );
+}
+// Akhir komponen header
+
+// Awal komponen main
+function Main({habits, onToggle, onDelete, onClear}){
+  const [sortBy, setSortBy] = useState('input');
+  let sortedItems;
+
+  switch (sortBy){
+    case 'name':
+      sortedItems = habits.slice().sort((a, b) => a.name.localeCompare(b.name));
+      break;
+    case 'checked':
+      sortedItems = habits.slice().sort((a, b) => a.checked - b.checked);
+      break;
+    default:
+      sortedItems = habits;
+      break;
+  }
+
+  return (
+    <main className="pt-4 pb-32"> 
+         <div className="container">
+          <div className="flex flex-wrap mx-4">
+            <Sort setSortBy={setSortBy} habits={habits} />
+            {sortedItems.map((habit) => (
+              <Item key={habit.id} habit={habit} onToggle={onToggle} onDelete={onDelete} />
+            ))}
+            <Clear onClear={onClear} habits={habits} />
+          </div>
+         </div>
+    </main>
+  );
+}
+
+function Item({habit, onToggle, onDelete}){
+  return (
+    <div className="w-full flex items-center p-4 my-2 bg-light rounded-lg shadow-sm">
+      <input type="checkbox" onChange={() => onToggle(habit.id)} checked={habit.checked} className="block w-[20px] h-[20px] mr-4" />
+      <h3 className="text-lg text-dark flex-1 truncate">{habit.name}</h3>
+      <button onClick={() => onDelete(habit.id)}><img src="/public/delete.png" alt="delete" /></button>
+    </div>
+  );
+}
+
+function Clear({onClear, habits}){
+  return (
+    <div className="w-full mt-4">
+      <button onClick={onClear}  style={habits.length === 0 ? {display: 'none'} : {}} className="block mx-auto px-4 py-2 bg-dark text-light rounded-full">clear all</button>
+    </div>
+  );
+}
+
+function Sort({setSortBy, habits}){
+  return (
+    <div className="w-full mb-4" style={habits.length === 0 ? {display: 'none'} : {}}>
+      <select onChange={(e) => setSortBy(e.target.value)} className="p-1 block mx-auto bg-light">
+        <option value="input">Berdasarkan input</option>
+        <option value="name">Berdasarkan nama</option>
+        <option value="checked">Berdasarkan ceklis</option>
+      </select>
+    </div>
+  );
+}
+
+// Akhir komponen main
+
+// Awal komponen footer
+function Footer({onAdd}){
+
+  return (
+    <footer className="fixed bottom-0 left-0 right-0 bg-secondary border-t-4 border-dark/40">
+        <div className="container">
+          <div className="flex flex-wrap mx-4">
+            <Input onAdd={onAdd} />
+          </div>
+        </div>
+    </footer>
+  );
+}
+
+function Input({onAdd}){
+  const [value, setValue] = useState('');
+
+  function handleClick(value){
+    onAdd(value);
+    setValue('');
+  }
+
+  return (
+    <div className="w-full flex items-center py-4">
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="block w-full bg-transparent border-b text-white text-lg mx-4 focus:outline-none" placeholder="Masukkan Kebiasaan Baru"/> 
+      <button onClick={() => handleClick(value)} style={value !== '' ? {display:'block'} : {}} className="hidden text-light text-2xl">&#10004;</button>
+    </div>
+  );
+}
+// Akhir komponen footer
