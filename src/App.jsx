d1c8c5df06942cba407324c477aa483b58481c53
WilbertFE <wilbertbernadi699@gmail.com>
2024-01-27 10:06:58 +0700

    Mengubah path tag img di app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -145,7 +145,7 @@ function Item({habit, onToggle, onDelete}){
     <div className="w-full flex items-center p-4 my-2 bg-light rounded-lg shadow-sm">
       <input type="checkbox" onChange={() => onToggle(habit.id)} checked={habit.checked} className="block w-[20px] h-[20px] mr-4" />
       <h3 className="text-lg text-dark flex-1 truncate">{habit.name}</h3>
-      <button onClick={() => onDelete(habit.id)}><img src="/public/delete.png" alt="delete" /></button>
+      <button onClick={() => onDelete(habit.id)}><img src="/habittracker/delete.png" alt="delete.png" /></button>
     </div>
   );
 }
